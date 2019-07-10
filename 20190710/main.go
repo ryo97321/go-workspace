e818bcfa0fa8a9a83d45cbe91d0a3acabbab5415
mHALr <ryo97321@gmail.com>
2019-07-10 12:54:59 +0900

    commit-2

--- 20190710/main.go
+++ 20190710/main.go
@@ -1,37 +1,15 @@
 package main
 
-import (
-	"image"
-	"image/color"
-	"image/jpeg"
-	"log"
-	"os"
-)
+import "fmt"
 
-func grad(img *image.RGBA) {
-	rect := img.Rect
-	for h := rect.Min.Y; h < rect.Max.Y; h++ {
-		for v := rect.Min.X; v < rect.Max.X; v++ {
-			img.Set(v, h, color.RGBA{
-				uint8(255 * h / rect.Max.Y),
-				uint8(255 * v / rect.Max.X),
-				uint8(255 * v * h / (rect.Max.X * rect.Max.Y)),
-				0})
-		}
-	}
+type Struct struct {
+	V float64
 }
 
 func main() {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	grad(img)
-
-	file, err := os.Create("test.jpg")
-	defer file.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := jpeg.Encode(file, img, &jpeg.Options{100}); err != nil {
-		log.Fatal(err)
-	}
+	var i1, i2, i3 interface{}
+	i1 = 12
+	i2 = "interface"
+	i3 = Struct{V: 3.14}
+	fmt.Println(i1, i2, i3)
 }
