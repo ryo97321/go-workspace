84e46ecb303342e8015bf3d3df4815006ecc4139
mHALr <ryo97321@gmail.com>
2019-07-25 18:00:48 +0900

    add search method

--- goUnzipper/main.go
+++ goUnzipper/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func searchZip(dirName string) []string {
+	files, _ := ioutil.ReadDir(dirName)
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, searchZip(filepath.Join(dirName, file.Name()))...)
+			continue
+		}
+		paths = append(paths, filepath.Join(dirName, file.Name()))
+	}
+
+	return paths
+}
+
 func main() {
 
+	zipFileNames := searchZip("./")
+
+	for _, name := range zipFileNames {
+		fmt.Println(name)
+	}
+
+	// files, _ := ioutil.ReadDir("./")
+
+	// for _, file := range files {
+	// 	fmt.Println(file.Name())
+	// if file.IsDir() != true {
+	// 	content, _ := ioutil.ReadFile(file.Name())
+	// 	fmt.Printf("Content : %s\n", string(content))
+	// }
+	// }
 }
