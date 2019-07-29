1cc568788558eea7b4a7659c632abab1d96ed2eb
mHALr <ryo97321@gmail.com>
2019-07-29 17:21:54 +0900

    add comment & change input password method

--- SQLTest/main.go
+++ SQLTest/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
+	"syscall"
 
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type Users struct {
@@ -16,36 +16,40 @@ type Users struct {
 }
 
 func main() {
-	stdin := bufio.NewScanner(os.Stdin)
-	fmt.Print("MySQL root password : ")
-	stdin.Scan()
-	password := stdin.Text()
 
+	// input MySQL password
+	fmt.Print("MySQL root password : ")
+	password, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println()
+
+	// make dataSourceName
 	dataSourceName := fmt.Sprintf("root:%s@/gosample", password)
 
+	// connect MySQL
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
 	fmt.Println("Connected.")
 
+	// Exec select Query
 	rows, err := db.Query("SELECT * FROM users")
 	defer rows.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// view Result
 	for rows.Next() {
 		var users Users
 		err := rows.Scan(&users.ID, &users.Name)
-
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		fmt.Println(users.ID, users.Name)
 	}
-
 }
