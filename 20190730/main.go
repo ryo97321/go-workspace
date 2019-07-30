90b4e352ba61e348e925fecaa2b16c41cbcd1424
mHALr <ryo97321@gmail.com>
2019-07-30 12:42:33 +0900

    paint system test

--- 20190730/main.go
+++ 20190730/main.go
@@ -10,15 +10,33 @@ import (
 
 func main() {
 	runewidth.DefaultCondition = &runewidth.Condition{EastAsianWidth: false}
-	goban.Main(app, view)
+	goban.Main(app)
 }
 
 func app(_ context.Context, es goban.Events) error {
-	goban.Show()
-	es.ReadKey()
-	return nil
+	v := func() {
+		b := goban.Screen()
+		b.Puts("Press any key to open popup")
+		b.Puts("Ctrl+C to exit")
+	}
+	goban.PushViewFunc(v)
+
+	for {
+		goban.Show()
+		es.ReadKey()
+		popup(es)
+	}
 }
 
-func view() {
-	goban.Screen().Enclose("hello").Prints("Hello World!\nPress any key to exit.")
+func popup(es goban.Events) {
+	v := func() {
+		b := goban.NewBox(0, 0, 40, 5).Enclose("popup window")
+		b.Prints("Press any key to popup")
+	}
+
+	goban.PushViewFunc(v)
+	defer goban.PopView()
+
+	goban.Show()
+	es.ReadKey()
 }
