ff7fa810ce8b9192c5a7d3c82e8bee9246003a40
mHALr <ryo97321@gmail.com>
2019-08-08 21:19:52 +0900

    test version -> mysql version

--- go-typing/main.go
+++ go-typing/main.go
@@ -2,17 +2,62 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
+	"syscall"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/ssh/terminal"
 )
+
+type Words struct {
+	ID   int
+	Word string
+}
+
+var testWords []string
+
+func getWordsFromDB() {
+	fmt.Print("MySQL root password : ")
+	password, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println()
+
+	dataSourceName := fmt.Sprintf("root:%s@/gotypingdb", password)
+
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM WORDS ORDER BY RAND() LIMIT 10")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var words Words
+		err := rows.Scan(&words.ID, &words.Word)
+		if err != nil {
+			log.Fatal(err)
+		}
+		testWords = append(testWords, words.Word)
+	}
+}
 
 func main() {
 	var userInput string
 	var answer string
-	var testWords = []string{"apple", "banana", "orange", "red", "blue", "green", "black", "white", "dog", "cat"}
 	var processTimeSecond float64
+
+	getWordsFromDB()
 
 	stdin := bufio.NewScanner(os.Stdin)
 	rand.Seed(time.Now().UnixNano())
@@ -22,7 +67,7 @@ func main() {
 
 	fmt.Println("********************")
 	for i := 0; i < 10; i++ {
-		answer = testWords[rand.Intn(10)]
+		answer = testWords[i]
 		fmt.Printf("%d : %s\n", (i + 1), answer)
 
 		startTime := time.Now()
