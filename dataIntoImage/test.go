7bd72b3adb294de14f6f95e4d3f19bdcc0872bff
mHALr <ryo97321@gmail.com>
2019-08-24 18:26:47 +0900

    add test file

---
+++ dataIntoImage/test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	// "01100111" -> 103
+	foo, _ := strconv.ParseInt("01100111", 2, 0)
+	fmt.Println(foo)
+
+	testStr := "01100111"
+	fmt.Println(testStr)
+
+	lastBitChangedTestStr := testStr[:7] + "0"
+	fmt.Println(lastBitChangedTestStr)
+	fmt.Println(string(lastBitChangedTestStr[0]))
+}
