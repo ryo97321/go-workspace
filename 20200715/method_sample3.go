6614f9cc7363b24449f4d60f87c0ded2f9adc85c
mHALr <ryo97321@gmail.com>
2020-07-15 21:14:55 +0900

    modified method_sample3.go

--- 20200715/method_sample3.go
+++ 20200715/method_sample3.go
@@ -11,25 +11,55 @@ type Human struct {
 type Student struct {
 	Human
 	school string
+	loan   float32
 }
 
 type Employee struct {
 	Human
 	company string
+	money   float32
 }
 
-func (h *Human) SayHi() {
+func (h Human) SayHi() {
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
-func (e *Employee) SayHi() {
+func (h Human) Sing(lyrics string) {
+	fmt.Println("La la, la la la, la la la la la...", lyrics)
+}
+
+func (e Employee) SayHi() {
 	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name, e.company, e.phone)
 }
 
-func main() {
-	mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
-	sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
+type Men interface {
+	SayHi()
+	Sing(lyrics string)
+}
 
-	mark.SayHi()
-	sam.SayHi()
+func main() {
+	mike := Student{Human{"Mike", 25, "222-222-XXX"}, "MIT", 0.00}
+	paul := Student{Human{"Paul", 26, "111-222-XXX"}, "Harvard", 100}
+	sam := Employee{Human{"Sam", 36, "444-222-XXX"}, "Golang Inc.", 1000}
+	tom := Employee{Human{"Tom", 37, "222-444-XXX"}, "Things Ltd.", 5000}
+
+	var i Men
+	i = mike
+	fmt.Println("This is Mike, a Student:")
+	i.SayHi()
+	i.Sing("November rain")
+
+	i = tom
+	fmt.Println("This is Tom, an Employee:")
+	i.SayHi()
+	i.Sing("Born to be wild")
+
+	fmt.Println("Let's use a slice of Men and see what happens")
+	x := make([]Men, 3)
+
+	x[0], x[1], x[2] = paul, sam, mike
+
+	for _, value := range x {
+		value.SayHi()
+	}
 }
