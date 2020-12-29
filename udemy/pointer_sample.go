97c18b6dbf6def32c53f7b265eb352a64ba9b0cf
mHALr <ryo97321@gmail.com>
2020-12-29 13:17:39 +0900

    add new line

--- udemy/pointer_sample.go
+++ udemy/pointer_sample.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
+func one(x *int) {
+	*x = 1
+}
+
 func main() {
 	var n int = 100
+	one(&n)
 	fmt.Println(n)
+	// fmt.Println(n)
 
-	fmt.Println(&n)
+	// fmt.Println(&n)
 
-	var p *int = &n
-	fmt.Println(p)
+	// var p *int = &n
+	// fmt.Println(p)
 
-	fmt.Println(*p)
+	// fmt.Println(*p)
 }
