f99b713fce47f72a9b1bdd78fe0300abae9dd366
mHALr <ryo97321@gmail.com>
2021-01-24 10:59:33 +0900

    add fyne_workspace/tour/canvas/image/

---
+++ fyne_workspace/tour/canvas/image/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/theme"
+)
+
+func main() {
+	myApp := app.New()
+	w := myApp.NewWindow("Image")
+
+	image := canvas.NewImageFromResource(theme.FyneLogo())
+	image.FillMode = canvas.ImageFillOriginal
+
+	w.SetContent(image)
+	w.ShowAndRun()
+}
