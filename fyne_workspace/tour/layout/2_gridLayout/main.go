ee7782f8a8ab908e5c38bf5f311e85eac457c35e
mHALr <ryo97321@gmail.com>
2021-01-26 09:40:59 +0900

    add 2_gridLayout/

---
+++ fyne_workspace/tour/layout/2_gridLayout/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+)
+
+func main() {
+	myApp := app.New()
+	w := myApp.NewWindow("Grid Layout")
+
+	text1 := canvas.NewText("1", color.Black)
+	text2 := canvas.NewText("2", color.Black)
+	text3 := canvas.NewText("3", color.Black)
+	grid := container.New(layout.NewGridLayout(2), text1, text2, text3)
+
+	w.SetContent(grid)
+	w.ShowAndRun()
+}
