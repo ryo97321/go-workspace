403cefc4401bc66bb43c342f36d9ad3e5ded1f6d
mHALr <ryo97321@gmail.com>
2021-02-01 11:33:28 +0900

    changed key-get process & result format

--- fyne_workspace/sandBox/twitterGUIDemo/main.go
+++ fyne_workspace/sandBox/twitterGUIDemo/main.go
@@ -1,44 +1,40 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/ChimeraCoder/anaconda"
 )
 
 func main() {
-	var accessToken = "accessToken"
-	var accessTokenSecret = "accessTokenSecret"
-	var consumerKey = "consumerKey"
-	var consumerSecret = "consumerSecret"
+	var accessToken = os.Getenv("twitterAPIAccessToken")
+	var accessTokenSecret = os.Getenv("twitterAPIAccessTokenSecret")
+	var consumerKey = os.Getenv("twitterAPIConsumerKey")
+	var consumerSecret = os.Getenv("twitterAPIConsumerSecret")
 
 	anaconda.SetConsumerKey(consumerKey)
 	anaconda.SetConsumerSecret(consumerSecret)
 	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
 	v := url.Values{}
-	v.Set("count", "3")
+	v.Set("count", "1")
+
 	tweets, err := api.GetHomeTimeline(v)
+
 	if err != nil {
 		fmt.Printf("Error to getHomeTimeline. err:%v\n", err)
 		os.Exit(1)
 	}
+
 	for i, tweet := range tweets {
+		createdTime, err := tweet.CreatedAtTime()
+		if err == nil {
+			jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+			createdTimeJST := createdTime.In(jst)
+			fmt.Println(createdTimeJST)
+		}
 		fmt.Printf("%d, TweetName:%v\nTweet:%v\n\n", i, tweet.User.Name, tweet.FullText)
 	}
-	for i, tweet := range tweets {
-		json, err := json.MarshalIndent(tweet, "", "  ")
-		if err != nil {
-			fmt.Printf("Error to json Marshal Error. err:%s\n", err)
-			os.Exit(1)
-		}
-		file, err := os.Create(fmt.Sprintf("timeline%d.json", i))
-		if err != nil {
-			fmt.Printf("Error to fileCreate. err:%v\n", err)
-		}
-		file.Write(json)
-	}
-
 }
