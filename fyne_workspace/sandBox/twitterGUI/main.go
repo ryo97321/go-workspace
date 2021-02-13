e5d076552461b218901c04ccb305aa52fe524bb3
mHALr <ryo97321@gmail.com>
2021-02-13 17:32:40 +0900

    add Get-userName_createdAtTime_fullText version code (developing)

--- fyne_workspace/sandBox/twitterGUI/main.go
+++ fyne_workspace/sandBox/twitterGUI/main.go
@@ -12,6 +12,56 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 )
 
+type MyTweetStruct struct {
+	username     string
+	createAtTime time.Time
+	fullText     string
+}
+
+// *** Now Developing ***
+// func getTweet() MyTweetStruct {
+
+// 	var accessToken = os.Getenv("twitterAPIAccessToken")
+// 	var accessTokenSecret = os.Getenv("twitterAPIAccessTokenSecret")
+// 	var consumerKey = os.Getenv("twitterAPIConsumerKey")
+// 	var consumerSecret = os.Getenv("twitterAPIConsumerSecret")
+
+// 	anaconda.SetConsumerKey(consumerKey)
+// 	anaconda.SetConsumerSecret(consumerSecret)
+// 	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+
+// 	v := url.Values{}
+// 	v.Set("count", "1")
+
+// 	myTweetStruct := MyTweetStruct{"", time.Now(), ""}
+
+// 	searchWord := "グラブル"
+// 	searchResult, err := api.GetSearch(searchWord, v)
+// 	if err != nil {
+// 		return myTweetStruct
+// 	}
+
+// 	tweetUserName := ""
+// 	tweetCreatedAtTime := time.Now()
+// 	tweetFullText := ""
+
+// 	if len(searchResult.Statuses) == 0 {
+// 		tweetFullText = "No Result"
+// 	} else {
+// 		for _, tweet := range searchResult.Statuses {
+// 			tweetUserName = tweet.User.Name
+// 			tweetCreatedAtTime, _ = tweet.CreatedAtTime()
+// 			tweetFullText = tweet.FullText
+// 		}
+// 	}
+
+// 	myTweetStruct.username = tweetUserName
+// 	myTweetStruct.createAtTime = tweetCreatedAtTime
+// 	myTweetStruct.fullText = tweetFullText
+
+// 	return myTweetStruct
+// }
+
 func getTweetFullText() string {
 
 	var accessToken = os.Getenv("twitterAPIAccessToken")
@@ -55,6 +105,32 @@ func setTweetPer10Seconds(text *canvas.Text, w fyne.Window) {
 		w.Show()
 	}
 }
+
+// *** Now Developing ***
+// func setTweetPer10Seconds(text *canvas.Text, w fyne.Window) {
+// 	for {
+// 		text.Refresh()
+
+// 		time.Sleep(time.Second * 10)
+// 		// text.Text = getTweetFullText()
+
+// 		myTweetStruct := getTweet()
+
+// 		text.Text = myTweetStruct.fullText
+// 		username := myTweetStruct.username
+// 		createdAtTime := myTweetStruct.createAtTime
+
+// 		usernameText := canvas.NewText(username, color.Black)
+// 		usernameText.Alignment = fyne.TextAlignCenter
+// 		createdAtTimeText := canvas.NewText(createdAtTime.String(), color.Black)
+// 		createdAtTimeText.Alignment = fyne.TextAlignCenter
+
+// 		content := container.New(layout.NewVBoxLayout(), createdAtTimeText, usernameText, text)
+
+// 		w.SetContent(content)
+// 		w.Show()
+// 	}
+// }
 
 func main() {
 	app := app.New()
