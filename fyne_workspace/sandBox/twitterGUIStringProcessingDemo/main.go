3c07580d9e8762ff98d625a967b4dd186a1f5768
mHALr <ryo97321@gmail.com>
2021-02-13 19:12:07 +0900

    add fyne.io code (not completed)

--- fyne_workspace/sandBox/twitterGUIStringProcessingDemo/main.go
+++ fyne_workspace/sandBox/twitterGUIStringProcessingDemo/main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 func getTweetFullTextLines(fullText string) []string {
@@ -18,4 +25,20 @@ func main() {
 	for _, line := range tweetFullTextLines {
 		fmt.Println(line)
 	}
+
+	app := app.New()
+	w := app.NewWindow("Title")
+
+	text1 := canvas.NewText("Text1", color.Black)
+	text1.Alignment = fyne.TextAlignCenter
+	text2 := canvas.NewText("Text2", color.Black)
+	text2.Alignment = fyne.TextAlignCenter
+	text3 := canvas.NewText("Text3", color.Black)
+	text3.Alignment = fyne.TextAlignCenter
+
+	content := container.New(layout.NewVBoxLayout(), text1, text2, text3)
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(300, 300))
+	w.ShowAndRun()
 }
