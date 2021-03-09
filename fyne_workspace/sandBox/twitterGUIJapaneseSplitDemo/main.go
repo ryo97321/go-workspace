fe9499e8ad01cd64fceca740d3efebc81317fb9f
mHALr <ryo97321@gmail.com>
2021-03-09 17:32:27 +0900

    add splitFullTextEvery30Chars()

--- fyne_workspace/sandBox/twitterGUIJapaneseSplitDemo/main.go
+++ fyne_workspace/sandBox/twitterGUIJapaneseSplitDemo/main.go
@@ -2,12 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-	s := "火曜日はTuesdayです。"
-	runes := []rune(s) // []string -> []rune
+// fullText を30文字ごとに分割する
+func splitFullTextEvery30Chars(runes []rune) []string {
+	var ss []string
 
-	for _, r := range runes {
-		c := string(r) // rune -> string
-		fmt.Println(c)
+	s := ""
+	for i, r := range runes {
+		s += string(r)
+
+		if (i+1)%30 == 0 { // 30文字になったらsをssに加え、sを空にする
+			ss = append(ss, s)
+			s = ""
+		}
+	}
+	if s != "" { // s に文字列が残っていたらssに加える
+		ss = append(ss, s)
+	}
+
+	return ss
+}
+
+func main() {
+	fullText := "月曜日はMondayです。火曜日はTuesdayです。水曜日はWednesdayです。"
+	runes := []rune(fullText) // []string -> []rune
+
+	fullTextLines := splitFullTextEvery30Chars(runes)
+	fmt.Printf("全%d行, %d文字\n", len(fullTextLines), len(runes))
+	for _, fullTextLine := range fullTextLines {
+		fmt.Printf("%s : %d文字\n", fullTextLine, len([]rune(fullTextLine)))
 	}
 }
