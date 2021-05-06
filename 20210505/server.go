7f71509991f9d4e90d1089fd7aebcd319e278c12
mHALr <ryo97321@gmail.com>
2021-05-06 19:41:43 +0900

    add some codes

--- 20210505/server.go
+++ 20210505/server.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
+
+type PostHelloRequest struct {
+	Name  string `json:name`
+	Times int    `json:times`
+}
+
+type PostHelloResponse struct {
+	Hellos []string `json:"hellos"`
+}
+
+func postHello(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var reqBody PostHelloRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	name, times := reqBody.Name, reqBody.Times
+
+	hellos := make([]string, 0, times)
+	for i := 0; i < times; i++ {
+		hellos = append(hellos, "Hello "+name)
+	}
+
+	resBody := PostHelloResponse{hellos}
+	encoder := json.NewDecoder(w)
+	encoder.SetIndent("", "	")
+	w.Header().Set("Content-Type", "application/json")
+	if err := encoder.Encode(resBody); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
 
 func getHello(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
