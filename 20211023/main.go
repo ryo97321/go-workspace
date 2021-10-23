46eaedbed5bbaf2f2e7513d6462fd0d94938659b
ryo97321 <ryo97321@gmail.com>
2021-10-23 14:03:45 +0900

    fixed 20211023/

--- 20211023/main.go
+++ 20211023/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello,World")
+	blocks := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+
+	for i := 0; i < len(blocks); i++ {
+		for j := 0; j < len(blocks[i]); j++ {
+			fmt.Printf("%d ", blocks[i][j])
+		}
+		fmt.Println()
+	}
 }
